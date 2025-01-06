e2af625deb866327bf41420045481b210bef7f95
Jose Carlos <jose890823@gmail.com>
2025-01-06 16:00:08 -0600

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  colorMode: {
+    classSuffix: ''
+  },
+  tailwindcss: {
+    // Opciones de tailwind si las necesitas
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+useHead({
+    title: "Home",
+});
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <Button>Click me</Button>
+    </div>
+</template>
